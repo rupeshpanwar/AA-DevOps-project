6c8e3d1095ba6d329c37f12ab2195e7ed15716ed
rupeshpanwar <rupeshpanwar28@gmail.com>
2021-02-12 15:03:15 +0530

    instance initialisation

---
+++ instances/instance.tf
@@ -0,0 +1,8 @@
+data "terraform_remote_state" "network-configuration" {
+  config = {
+    bucket = var.remote_state_bucket
+    key = var.remote_state_key
+    region = var.region
+  }
+  backend = "s3"
+}
---
+++ instances/production.tfvars
@@ -0,0 +1,3 @@
+remote_state_bucket =  "terraform-remote-state-09-02-2021"
+remote_state_key = "acklendemo.tfstate"
+
