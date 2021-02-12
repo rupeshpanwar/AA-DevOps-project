41d9a9f6634f1ace22805d92a4487303ed25cc63
rupeshpanwar <rupeshpanwar28@gmail.com>
2021-02-12 14:20:14 +0530

    Create Subets

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,6 @@
 output "vpc_id" {
   value = aws_vpc.mainvpc.id
 }
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets.*.id
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,19 @@
 variable "vpc_cidr" {
   description = "Production VPC CIDR"
 }
+
+variable "public_subnet_cidr" {
+  type = list(string)
+  description = "Subnet CIDR"
+}
+
+variable "public_subnet_names" {
+  type = list(string)
+  description = "Subnet names"
+}
+
+variable "availability_zone" {
+  type = list(string)
+  description = "Availability zones"
+}
+
